c2c8e88f389c2fa3d680aecbb0e29402abc1e889
mika <mikagivner123@gmail.com>
2023-02-01 19:51:36 +0200

    mika-js

---
+++ worldMap.js
@@ -0,0 +1,10 @@
+const MapSelectors = {
+  potionAmount: document.querySelector(".countPotion"),
+  goldAmount: document.querySelector(".goldAmount"),
+  lifeAmount: document.querySelector(".lifeAmount"),
+  toLocation1: document.querySelector(".toLocation1"),
+  toLocation2: document.querySelector(".toLocation2"),
+  toLocation3: document.querySelector(".toLocation3"),
+  toLocation4: document.querySelector(".toLocation4"),
+  backTostats: document.querySelector("#backTostats"),
+};
