13e958c68821ea082900134b47b126be35ebd2fe
eladjmc <eladjmc88@gmail.com>
2023-02-01 12:11:30 +0200

    added monster to my branch

--- elad.js
+++ elad.js
@@ -1,3 +1,17 @@
+
+class Monster {
+    constructor(maxHp, currentHp, name, str, dex, gold, lvl, def) {
+        this.maxHp = maxHp;
+        this.currentHp = currentHp;
+        this.name = name;
+        this.str = str;
+        this.dex = dex;
+        this.gold = gold;
+        this.lvl = lvl;
+        this.def = def;
+    }
+}
+
 function genMonster() {
 
 }
